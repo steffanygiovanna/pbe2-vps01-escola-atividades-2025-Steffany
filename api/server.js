89aa5ab90793f537d0e1d6f9d4d1ab2a3bf5ec3d
steffanygiovanna <00001108194278sp@al.educacao.sp.gov.br>
2025-04-16 09:48:15 -0300

    .

--- api/server.js
+++ api/server.js
@@ -8,6 +8,6 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3001, () => {
-  console.log('API executando em http://localhost:3001');
+app.listen(5000, () => {
+  console.log('API executando em http://localhost:3306');
 });
